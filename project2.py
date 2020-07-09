a4a7244ec0e243f1c6405e030394165843bafc6c
burhan <burhan420@gmail.com>
2020-07-09 17:00:47 -0400

    epipolar line drawing

--- project2.py
+++ project2.py
@@ -10,7 +10,9 @@ def img1ClickHandler(event):
         canvas1.create_oval(x-5,y-5,x+5,y+5,fill=dot_color)
     # print("img1:",imgPoints1)
     if len(imgPoints1) == 10 and len(imgPoints2) == 10:
-        epipolarMode.state(['!disabled'])
+        modeChk.state(['!disabled'])
+    if epipolarMode.get():
+        drawEpipolarLine(event.x,event.y)
     
 def img2ClickHandler(event):
     if len(imgPoints2)<10:
@@ -19,7 +21,7 @@ def img2ClickHandler(event):
         canvas2.create_oval(x-5,y-5,x+5,y+5,fill=dot_color)
     # print("img2:",imgPoints2)
     if len(imgPoints1) == 10 and len(imgPoints2) == 10:
-        epipolarMode.state(['!disabled'])
+        modeChk.state(['!disabled'])
     
 def createInputMatrix():
     for i in range(len(imgPoints1)):
@@ -27,17 +29,32 @@ def createInputMatrix():
         q = imgPoints2[i]
         matrixA.append([q[0]*p[0],q[0]*p[1],q[0],q[1]*p[0],q[1]*p[1],q[1],p[0],p[1],1])
 def computeFundamentalMatrix():
-    if len(imgPoints1) < 10 or len(imgPoints2) < 10:
-        return
-    canvas1.delete('all')
-    canvas2.delete('all')
-    createInputMatrix()
-    U, s, vt = svd(matrixA)
-    minEigenValue = np.argmin(s)
-    matrixF = vt[minEigenValue].reshape(3,3)
-    print(matrixF)
+    print("mode chck:",epipolarMode.get())
+    # if len(imgPoints1) < 10 or len(imgPoints2) < 10:
+    #     return
+    if epipolarMode.get():
+        canvas1.delete('all')
+        canvas2.delete('all')
+        createInputMatrix()
+        U, s, vt = svd(matrixA)
+        minEigenValue = np.argmin(s)
+        global matrixF
+        matrixF = vt[minEigenValue].reshape(3,3)
+        print(matrixF)
     
 
+def computeEpipolarPoints(u,v):
+    print(matrixF)
+    f1 = matrixF[0]
+    f2 = matrixF[1]
+    f3 = matrixF[2]
+    u1 = -((v+100)*(v*f2[0]+v*f2[1]+f2[2])+u*f3[0]+v*f3[1]+f3[2])/(u*f1[0]+v*f1[1]+f1[2])
+    u2 = -((v-100)*(v*f2[0]+v*f2[1]+f2[2])+u*f3[0]+v*f3[1]+f3[2])/(u*f1[0]+v*f1[1]+f1[2])
+    return u1,(v+100),u2,(v-100)
+def drawEpipolarLine(u,v):
+    u1,v1,u2,v2 = computeEpipolarPoints(u,v)
+    print("epi points",u1,v1,u2,v2)
+    canvas2.create_line(u1,v1,u2,v2,fill='green',width=5)
 if __name__ == "__main__":
     imgPoints1 = []
     imgPoints2 = []
@@ -64,11 +81,13 @@ if __name__ == "__main__":
     frame3.grid(row=1,column=0)
     frame3.config()
 
-    epipolarMode = ttk.Checkbutton(frame3, text="Epiploar Mode")
-    epipolarMode.config(command = computeFundamentalMatrix)
-    epipolarMode.pack() 
+    epipolarMode = BooleanVar()
+    modeChk = ttk.Checkbutton(frame3, text="Epiploar Mode", variable=epipolarMode)
+    modeChk.config(command = computeFundamentalMatrix)
+
+    modeChk.pack() 
     if len(imgPoints1) < 10 or len(imgPoints2) < 10:
-        epipolarMode.state(['disabled'])
+        modeChk.state(['disabled'])
 
 
     main_window.mainloop()
