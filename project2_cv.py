47cc4ae3a5581460063c33b540ac16e2e7d04013
burhan <burhan420@gmail.com>
2020-08-06 12:27:51 -0400

    2nd submission

--- project2_cv.py
+++ project2_cv.py
@@ -4,64 +4,57 @@ import numpy as np
 from scipy.linalg import svd
 from tkinter import filedialog
 from PIL import ImageTk, Image
+import cv2
+
 
 def img1ClickHandler(event):
     if len(imgPoints1)<10:
         x,y = event.x,event.y
-        print("first: ",x,y)
-        imgPoints1.append([x,y])
+        imgPoints1.append((x,y))
+        print("first: ",x,y,len(imgPoints1))
         canvas1.create_oval(x-5,y-5,x+5,y+5,fill=dot_color, tags='dots')
     # print("img1:",imgPoints1)
     if len(imgPoints1) == 10 and len(imgPoints2) == 10:
         modeChk.state(['!disabled'])
     if epipolarMode.get():
-        x,y = event.x,event.y
-        canvas1.create_oval(x-5,y-5,x+5,y+5,fill=dot_color, tags='dots')
-        drawEpipolarLine(x,y)
+        drawEpipolarLine(event.x,event.y)
     
 def img2ClickHandler(event):
     if len(imgPoints2)<10:
         x,y = event.x,event.y
-        print("second: ",x,y)
-        imgPoints2.append([x,y])
+        imgPoints2.append((x,y))
+        print("second: ",x,y,len(imgPoints2))
         canvas2.create_oval(x-5,y-5,x+5,y+5,fill=dot_color, tags='dots')
     # print("img2:",imgPoints2)
     if len(imgPoints1) == 10 and len(imgPoints2) == 10:
         modeChk.state(['!disabled'])
     
-def createInputMatrix():
-    for i in range(len(imgPoints1)):
-        p = imgPoints1[i]
-        q = imgPoints2[i]
-        matrixA.append([q[0]*p[0],q[0]*p[1],q[0],q[1]*p[0],q[1]*p[1],q[1],p[0],p[1],1])
 def computeFundamentalMatrix():
     print("mode chck:",epipolarMode.get())
-    # if len(imgPoints1) < 10 or len(imgPoints2) < 10:
-    #     return
     if epipolarMode.get():
         canvas1.delete('dots')
         canvas2.delete('dots')
         print(imgPoints1)
-        createInputMatrix()
-        U, s, vt = svd(matrixA)
-        minEigenValue = np.argmin(s)
+        pts1 = np.int32(imgPoints1)
+        pts2 = np.int32(imgPoints2)
         global matrixF
-        matrixF = vt[minEigenValue].reshape(3,3)
+        matrixF, mask = cv2.findFundamentalMat(pts1,pts2,cv2.FM_LMEDS)
         print(matrixF)
-    
+        print(matrixF[0][0])
 
-def computeEpipolarPoints(u,v):
-    # print(matrixF)
-    f1 = matrixF[0]
-    f2 = matrixF[1]
-    f3 = matrixF[2]
+def computeEpipolarPoints(u,v,fMatrix):
+    print(matrixF)
+    f1 = fMatrix[0]
+    f2 = fMatrix[1]
+    f3 = fMatrix[2]
     u1 = -(0*(v*f2[0]+v*f2[1]+f2[2])+u*f3[0]+v*f3[1]+f3[2])/(u*f1[0]+v*f1[1]+f1[2])
     u2 = -(1080*(v*f2[0]+v*f2[1]+f2[2])+u*f3[0]+v*f3[1]+f3[2])/(u*f1[0]+v*f1[1]+f1[2])
     return u1,0,u2,1080
-    
+
 def drawEpipolarLine(u,v):
-    u1,v1,u2,v2 = computeEpipolarPoints(u,v)
-    canvas2.create_line(u1,v1,u2,v2,fill='green',width=5)
+    u1,v1,u2,v2 = computeEpipolarPoints(u,v,matrixF)
+    canvas2.create_line(u1,v1,u2,v2,fill='red',width=5)
+
 def selectImg1():
     imgPoints1.clear()
     imgPoints2.clear()
@@ -81,15 +74,46 @@ def selectImg2():
     canvas2.img = ImageTk.PhotoImage(Image.open(img2Url).resize((1920,1080)))
     canvas2.create_image(0, 0, image=canvas2.img, anchor="nw")
 
+def computeFundamentalMatCV():
+    img1 = cv2.imread('/home/burhan/Desktop/20200714_170554.jpg',0)  #queryimage # left image
+    img2 = cv2.imread('/home/burhan/Desktop/20200714_170558.jpg',0) #trainimage # right image
+    sift = cv2.xfeatures2d.SIFT_create()
+    # find the keypoints and descriptors with SIFT
+    kp1, des1 = sift.detectAndCompute(img1,None)
+    kp2, des2 = sift.detectAndCompute(img2,None)
+
+    # FLANN parameters
+    FLANN_INDEX_KDTREE = 0
+    index_params = dict(algorithm = FLANN_INDEX_KDTREE, trees = 5)
+    search_params = dict(checks=50)
+
+    flann = cv2.FlannBasedMatcher(index_params,search_params)
+    matches = flann.knnMatch(des1,des2,k=2)
+    good = []
+    pts1 = []
+    pts2 = []
+    # ratio test as per Lowe's paper
+    for i,(m,n) in enumerate(matches):
+        if m.distance < 0.8*n.distance:
+            good.append(m)
+            pts2.append(kp2[m.trainIdx].pt)
+            pts1.append(kp1[m.queryIdx].pt)
+    pts1 = np.int32(pts1)
+    pts2 = np.int32(pts2)
+    global F 
+    F, mask = cv2.findFundamentalMat(pts1,pts2,cv2.FM_LMEDS)
+    print(F)
+
 if __name__ == "__main__":
     imageDir = "/home/burhan/Desktop/"
     imgPoints1 = []
     imgPoints2 = []
-    matrixA = []
+    # matrixA = []
     # matrixF = []
-    dot_color = "#FF0000"
+    # computeFundamentalMatCV()
+    dot_color = "#476042"
     main_window = Tk()
-    main_window.title("Project 2")
+    main_window.title("Project 2 with OpenCV")
 
     button1 = ttk.Button(main_window,text="Select 1st image",command=selectImg1)
     button2 = ttk.Button(main_window,text="Select 2nd image",command=selectImg2)
@@ -129,6 +153,13 @@ if __name__ == "__main__":
     # print("img2:",imgPoints2)
 
 
-# [[-2.78119151e-08 -6.59734003e-06  5.16262124e-03]
-#  [ 2.58666671e-06  2.74851330e-07 -1.46301729e-01]
-#  [-6.10334920e-03  1.49293048e-01  9.77877030e-01]]
+
+
+
+# [[ 1.95962208e-06 -8.19836584e-05  2.64289856e-01]
+#  [ 8.08639085e-05 -3.96141339e-07 -7.67321249e-02]
+#  [-2.68911957e-01  7.96023215e-02  1.00000000e+00]]
+
+# [[ 2.00517156e-08  6.46929244e-06 -1.72202368e-02]
+#  [-5.87467013e-06  2.92222259e-07  1.41722732e-01]
+#  [ 1.52138984e-02 -1.43034621e-01  1.00000000e+00]]
